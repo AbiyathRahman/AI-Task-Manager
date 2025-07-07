c4d2cf210b06d87751f70f8b51dd936613667dbe
Abiyath Rahman <abiyathrahman@mail.weber.edu>
2025-07-07 11:20:12 -0600

    Created env variables for database. Using neon databse which is serverless.Created docker image and built it and pushed into the repo. Deployed backend on render using docker image.

--- InsightPulse/src/main/java/com/insightpulse/InsightPulse/InsightPulseApplication.java
+++ InsightPulse/src/main/java/com/insightpulse/InsightPulse/InsightPulseApplication.java
@@ -1,12 +1,24 @@
 package com.insightpulse.InsightPulse;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class InsightPulseApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		String dbUrl = dotenv.get("DATASOURCE_URL");
+		String dbUser = dotenv.get("DATASOURCE_USERNAME");
+		String dbPass = dotenv.get("DATASOURCE_PASSWORD");
+
+		if (dbUrl != null) System.setProperty("DATASOURCE_URL", dbUrl);
+		if (dbUser != null) System.setProperty("DATASOURCE_USERNAME", dbUser);
+		if (dbPass != null) System.setProperty("DATASOURCE_PASSWORD", dbPass);
+
 		SpringApplication.run(InsightPulseApplication.class, args);
 	}
 
